856047dc4ce87d69633a4f9b4ae536e0d1e9051b
tj123 <tang123jian@126..com>
2017-04-02 10:53:35 +0800

    New changelist

--- service/src/main/java/com/shundian/frame/config/MyBatisMapperScannerConfig.java
+++ service/src/main/java/com/shundian/frame/config/MyBatisMapperScannerConfig.java
@@ -46,18 +46,13 @@ public class MyBatisMapperScannerConfig {
     public MapperScannerConfigurer mapperScannerConfigurer() {
         MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
         mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
-        //mapperScannerConfigurer.setBasePackage("tk.mybatis.springboot.mapper");
-        mapperScannerConfigurer.setBasePackage("com.shundian.frame.mapper");
+        mapperScannerConfigurer.setBasePackage("tk.mybatis.springboot.mapper");
         Properties properties = new Properties();
-
-        //properties.setProperty("mappers", "tk.mybatis.springboot.util.MyMapper");
-
-
+//        properties.setProperty("mappers", "com.shundian.frame.mapper");
         properties.setProperty("notEmpty", "false");
         properties.setProperty("IDENTITY", "MYSQL");
         mapperScannerConfigurer.setProperties(properties);
         return mapperScannerConfigurer;
     }
-
 
 }
---
+++ web/src/main/java/com/shundian/frame/task/RunTask.java
@@ -0,0 +1,7 @@
+package com.shundian.frame.task;
+
+/**
+ * Created by TJ on 2017/2/12.
+ */
+public class RunTask {
+}
