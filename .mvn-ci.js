ca103b385d290d27610c088df0d93f6c96dacca4
tj <tang123jian@126.com>
2018-02-06 18:04:38 +0800

    execute

---
+++ .mvn-ci.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+var cfg = {
+
+};
+
+
+
+
+
+
+
+
+
+
+
+var config = {
+
+  env: 'test',
+  tmpDir:'/tmp/asfasdgasefdd',
+  middleServer:'',
+
+
+
+};
+
+
+
+
+
+
+module.exports = cfg;
+
